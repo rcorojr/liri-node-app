d29eecf554616e5c108ede1552584e8f75cd7793
rcorojr <61759545+rcorojr@users.noreply.github.com>
2020-06-12 14:33:12 -0600

    add ignore files. add txt file.include linke to key file

---
+++ liri.js
@@ -0,0 +1,42 @@
+//required packages
+
+var dotenv = require('dotenv').config()
+var keys = require("./keys.js");
+var axios = require("axios")
+var spotify = require("node-spotify-api")
+var moment = require ("moment")
+
+//Access Spotify
+var spotify = new Spotify(keys.spotify);
+
+//movie search
+
+var movieName = process.argv[2];
+
+var queryUrlMov = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=1fb3acc8";
+
+console.log(queryUrlMov);
+
+
+axios.get(queryUrlMov).then(
+    function(response){
+        console.log("The movie's release date is: " + response.data.Released);
+    }
+)
+.catch(function(error){
+    if (error.response) {
+
+        console.log("---------------Data---------------");
+        console.log(error.response.data);
+        console.log("---------------Status---------------");
+        console.log(error.response.status);
+        console.log("---------------Status---------------");
+        console.log(error.response.headers);
+      } else if (error.request) {
+
+        console.log(error.request);
+      } else {
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+    });
