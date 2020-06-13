be2c8f8a6409ddd0e15975767f48741a77589d5d
rcorojr <61759545+rcorojr@users.noreply.github.com>
2020-06-13 16:46:27 -0600

    modify movie search. add switch, userInput, and userTopic

--- liri.js
+++ liri.js
@@ -6,21 +6,38 @@ var axios = require("axios")
 var spotify = require("node-spotify-api")
 var moment = require ("moment")
 
-// // //Access Spotify
-// var spotify = new Spotify(keys.spotify);
+var userInput = process.argv;
+var userTopic = process.argv[2];
+
+//topic selector
+switch (userTopic){
+  case "movie-this":
+    movieData();
+    break;
+}
 
 //movie search
 
-var movieName = process.argv[2];
+// var movieName = process.argv[2];
 
 var queryUrlMov = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=1fb3acc8";
 
-
+function movieData(){
+  var movieName = "";
+  for (var i = 3; i < userInput.length; i++){
+      if (i > 3 && i < userInput.length){
+          movieName = movieName + "+" + userInput[i];
+      }
+      else{
+          movieName += userInput[i];
+      }
+  }
+}
 
 axios.get(queryUrlMov).then(
     function(response){
       console.log("Title: " + response.data.Title);  
-      console.log("Release Year: " + response.data.Released);
+      console.log("Release Year: " + responseswitch .data.Released);
       console.log("IMDB Rating: " + response.data.imdbRating);
       console.log("Rotton Tomatoes rating: " + response.data.Ratings[1].Value);
       console.log("Country produced: " + response.data.Country);
@@ -29,6 +46,12 @@ axios.get(queryUrlMov).then(
       console.log("Actors: " + response.data.Actors);
     }
 )
+
+// // //Access Spotify
+// var spotify = new Spotify(keys.spotify);
+
+
+
 .catch(function(error){
     if (error.response) {
 
